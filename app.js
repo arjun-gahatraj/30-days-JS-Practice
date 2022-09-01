09d304d29d92b91843e10ddeb703c48aa8e41c1f
unknown <arjungr8898@gmail.com>
2022-09-01 09:21:34 +0530

    fixed the merge issue

--- app.js
+++ app.js
@@ -1,37 +1,25 @@
-// //function to check if the array is of same data type.
-// function checkType(param){
-// //stored the data type of 1st element to compare it with other elements, if the param[0] is not same as param[1] it will return not same.
-//     let firstElemType = typeof(param[0]);
-
-// //let i = 1; because we have already stored param[0] if i use let i = 0, it will check the firstElemType data type again and if i have something as ["a",1,2,3,4,5] this will return true regardless of different data type. hence we check the 0th element data type with 1st element and continue
-//     for(let i = 1; i < param.length; i++){
-//         if(firstElemType === typeof(param[i])){
-//             return "Data type is same for all elelemts"
-//         } else{
-//             return "Data type is not same"
-//         }
-//      }
-// }
-// console.log(checkType([1,2,3,4,5,6]))
-
-// const checkVarName = (param) => {
-//     let regEx = /[^A-z|$|_]/ //this will check wether these characters are there if it has any other characters other than those mentioned in the regex if we use .test it will return true if it finds any other character or it will return false if no new characters other than A-z|$|_ are found it will return false. ^ is used to find something that is NOT in a given data.
-//     let matchUsingTest = regEx.test(param)
-//         if(matchUsingTest === true){
-//             return "invalid variable name"
-//         }
-//         else{
-//             return "valid variable name"
-//         }  
-// }
-// console.log(checkVarName("full_Name"))
+function checkType(param){
+    //stored the data type of 1st element to compare it with other elements, if the param[0] is not same as param[1] it will return not same.
+    let firstElemType = typeof(param[0]);
+    let storage = ""
+    //let i = 1; because we have already stored param[0] if i use let i = 0, it will check the firstElemType data type again and if i have something as ["a",1,2,3,4,5] this will return true regardless of different data type. hence we check the 0th element data type with 1st element and continue
+        for(let i = 1; i < param.length; i++){
+             if(firstElemType === typeof(param[i])){
+                 storage = "Data type is same" // if data types are same keep updating the storage variable i.e. Data type is same. if not move to else part.
+             } else{
+               return storage = "Data type is not same"; // if the data type is not same as compared to the 1st element data type just retun Data type is not same,
+            }
+          }
+          return storage;
+     }
+     console.log(checkType([,"a",1,2,3,4,5,6]));
 
 
 //function that returns array of seven random numbers from range 0-9 and all unique
 function sevenRandomNums(nums){
     let store = [];
     let i = 0;
-    while(store.length < 7){ //< 7 coz i want 7 random numbs on my store variable. And as this is a while loop it will run untill store is equal to 7
+    while(store.length < 7){ //< run untill storage length is 7. So that is how we get store to have 7 random numbers from 0-9, this will run untill the 7 numbers are unique. and stop when store length is 7
         let randomNumGen = Math.floor(Math.random() * nums.length);
         if(store.indexOf(nums[randomNumGen]) === -1){ // 
             store.push(nums[randomNumGen]);
@@ -40,25 +28,19 @@ function sevenRandomNums(nums){
     }
     return store;
 }
-<<<<<<< HEAD
 console.log(sevenRandomNums([0,1,2,3,4,5,6,7,8,9]))
-=======
-console.log(checkType([1,2,3,4,5,6]))
 
 //Check weather the given variable name is valid or not
 const checkVarName = (param) => {
-    let regEx = /[^A-z|$|_]/ //this will check wether these characters are there if it has any other characters other than those mentioned in the regex if we use .test it will return true if it finds any other character or it will return false if no new characters other than A-z|$|_ are found it will return false. ^ is used to find something that is NOT in a given data.
+    let regEx = /[^A-z|$|_]/ //this regex will include everything other than A-z or $ or _ if it encounters any other characters other than those mentioned it will return True as we are using .test method. it returns Boolean value.
     let matchUsingTest = regEx.test(param)
-        if(matchUsingTest === true){
+        if(matchUsingTest === true){ //here we are testing if the param has any other characs than mentioned in regEx variable if yes then it will give true(yes there are characters other than mentioned in the regEx variable, so it will be invalid)
             return "invalid variable name"
         }
-        else{
+        else{//if the param has no other characters other than mentidoned in the regEx variable, it will evaluate to false and then this else will get evaluated
             return "valid variable name"
         }  
 }
 
-console.log(checkVarName("full_Name"))
+console.log(checkVarName("_full_Name"))
 
-
-
->>>>>>> c6d2e866394d65cb3b57a0a95c9714dd273d5b95
